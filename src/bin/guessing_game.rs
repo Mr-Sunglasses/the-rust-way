e71c2e0e4a5ff192f7c55a62cdf46c15e8eb80c2
Kanishk Pachauri <itskanishkp.py@gmail.com>
2026-04-26 02:29:35 +0530

    chore: explain more in guessing game

--- src/bin/guessing_game.rs
+++ src/bin/guessing_game.rs
@@ -5,19 +5,21 @@ fn main() {
     println!("Guess the number");
     let mut rng = rand::rng();
     // can do done also like rand::random_range(1..=100);
-    let secret_number = rng.random_range(1..=100); // in rust book it is rand::thread_rng().gen_range(1..=100) which is in some old version of rand
+    let secret_number: u32 = rng.random_range(1..=100); // in rust book it is rand::thread_rng().gen_range(1..=100) which is in some old version of rand
 
     loop {
         println!("Please input the number you guess: ");
 
         let mut guess = String::new();
 
+        // to get user input and send it to guess
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
-            Ok(some) => some,
+            // check is mut guess which is parse is of type u32, if yes assign the value to the var and if not, continue
+            Ok(some) => some, // here some is a comparison place holder, some can be any or num or any name you want
             Err(_) => continue,
         };
 
